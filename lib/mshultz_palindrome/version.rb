5ccdbc1700b65372aabb954374f4ca740a03257f
Matt Shultz <teamshultz@gmail.com>
2022-04-07 18:00:39 -0500

    Initial commit

---
+++ lib/mshultz_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MshultzPalindrome
+  VERSION = "0.1.0"
+end
---
+++ test/mshultz_palindrome_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class MshultzPalindromeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::MshultzPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
